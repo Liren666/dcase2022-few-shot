e2d548dd11bce1aaa35bfb3f2936b3bef72152b3
liren <1105342352@qq.com>
2022-08-14 12:07:33 +0100

    Update Autoencoder model

--- Model.py
+++ Model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 import torch
 from collections import OrderedDict
 
@@ -11,6 +12,16 @@ def conv_block(in_channels,out_channels):
         nn.ReLU(),
         nn.MaxPool2d(2)
     )
+
+def convTranspose_block(in_channels,out_channels):
+
+    return nn.Sequential(
+        nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, padding=0 , stride=2, output_padding=0),
+        nn.BatchNorm2d(out_channels),
+        nn.Conv2d(out_channels, out_channels, 3, padding=1),
+        nn.ReLU()
+    )
+
 
 class ProtoNet(nn.Module):
     def __init__(self):
@@ -26,7 +37,7 @@ class ProtoNet(nn.Module):
         x = x.view(-1,1,seq_len,mel_bins)
         x = self.encoder(x)
         x = nn.MaxPool2d(1)(x)
-        
+
         return x.view(x.size(0),-1)
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -44,6 +55,7 @@ class BasicBlock(nn.Module):
         self.relu = nn.LeakyReLU(0.1)
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = conv3x3(planes, planes)
         self.conv3 = conv3x3(planes, planes)
         self.bn3 = nn.BatchNorm2d(planes)
         self.maxpool = nn.MaxPool2d(stride)
@@ -77,15 +89,14 @@ class BasicBlock(nn.Module):
         out = self.relu(out)
         out = self.maxpool(out)
         out = F.dropout(out, p=self.drop_rate, training=self.training, inplace=True)
-        
-                
 
         return out
 
 
+
 class ResNet(nn.Module):
 
-    def __init__(self, block=BasicBlock, keep_prob=1.0, avg_pool=True, drop_rate=0.1, dropblock_size=5):
+    def __init__(self, block=BasicBlock, unblook= BasicBlock, keep_prob=1.0, avg_pool=True, drop_rate=0.1, dropblock_size=5):
         self.inplanes = 1
         super(ResNet, self).__init__()
 
@@ -123,6 +134,7 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
+
     def forward(self, x):
         (num_samples,seq_len,mel_bins) = x.shape
         x = x.view(-1,1,seq_len,mel_bins)
@@ -134,7 +146,105 @@ class ResNet(nn.Module):
         #x = self.layer4(x)
         
         x = self.pool(x)
-        
+
         x = x.view(x.size(0), -1)
-        
+
+        return x
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, retain_activation=True, activation='ReLU'):
+        super(ConvBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+
+        if retain_activation:
+            if activation == 'ReLU':
+                self.block.add_module("ReLU", nn.ReLU(inplace=True))
+            elif activation == 'LeakyReLU':
+                self.block.add_module("LeakyReLU", nn.LeakyReLU(0.1))
+            elif activation == 'Softplus':
+                self.block.add_module("Softplus", nn.Softplus())
+        self.block.add_module("MaxPool2d", nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+
+    def forward(self, x):
+        out = self.block(x)
+        return out
+
+
+class ConvTransposeBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, output_padding=0, retain_activation=True, activation='ReLU'):
+        super(ConvTransposeBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2, padding=0,
+                               output_padding=output_padding),
+            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        )
+
+        if retain_activation:
+            if activation == 'ReLU':
+                self.block.add_module("ReLU", nn.ReLU(inplace=True))
+            elif activation == 'LeakyReLU':
+                self.block.add_module("LeakyReLU", nn.LeakyReLU(0.1))
+            elif activation == 'Softplus':
+                self.block.add_module("Softplus", nn.Softplus())
+
+    def forward(self, x):
+        out = self.block(x)
+        return out
+
+
+class AutoProtoNetEmbedding(nn.Module):
+    """ Model consists of an encoder and decoder
+    """
+
+    def __init__(self):
+        super(AutoProtoNetEmbedding, self).__init__()
+        self.encoder = nn.Sequential(
+            conv_block(1, 64),
+            conv_block(64, 64),
+            conv_block(64, 64),
+            conv_block(64, 64)
+        )
+
+        self.decoder = nn.Sequential(
+            convTranspose_block(64, 64),
+            convTranspose_block(64, 64),
+            convTranspose_block(64, 64),
+            convTranspose_block(64, 1)
+        )
+
+        self.embedding_shape = None
+
+
+    def forward(self, x):
+        """ Forward on the AutoProtoNet produces an embedding
+        """
+        (num_samples, seq_len, mel_bins) = x.shape
+        x = x.view(-1, 1, seq_len, mel_bins)
+        #print(x.size())
+        x = self.encoder(x)
+        #x = x.view(-1, 1, x.size(2), 128)
+        out_1 = x.view(x.size(0),-1)
+        #print(out_1.size())
+        return out_1
+
+    def forward_decoder(self, e):
+        (num_samples, seq_len, mel_bins) = e.shape
+        e = e.view(-1, 1, seq_len, mel_bins)
+        e = self.encoder(e)
+        e = self.decoder(e)
+
+        #e = nn.MaxPool2d(2)(e)
+        out_2 = e.view(e.size(0), -1)
+        return out_2
+
+    def forward_plus_decoder(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
