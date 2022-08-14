c0d264f1cbf3ea14bebc1f715660f7468eda9dcc
liren <1105342352@qq.com>
2022-08-14 11:55:52 +0100

    Add Autoloss

--- util.py
+++ util.py
@@ -2,14 +2,14 @@ import torch
 from torch.nn import functional as F
 from torch.nn.modules import Module
 from torch.utils.data import DataLoader
-from Model import ProtoNet, ResNet
+from Model import ProtoNet, ResNet, AutoProtoNetEmbedding
 from Datagenerator import Datagen_test
 import numpy as np
 from batch_sampler import EpisodicBatchSampler
 from tqdm import tqdm
 from scipy.ndimage.filters import maximum_filter1d, minimum_filter
 from torch.autograd import Variable
-
+lambda_r = 1
 
 def euclidean_dist(x, y):
     '''
@@ -44,7 +44,7 @@ def prototypical_loss(input, target, n_support):
     - input: the model output for a batch of samples
     - target: ground truth for the above batch of samples
     - n_support: number of samples to keep in account when computing
-      bprototypes, for each one of the current classes
+      prototypes, for each one of the current classes
     '''
 
     def supp_idxs(c):
@@ -84,12 +84,76 @@ def prototypical_loss(input, target, n_support):
 
     return loss, acc_val
 
+def Autoproto_loss(input, recon, ground, target, n_support):
+    '''
+    Adopted from https://github.com/orobix/Prototypical-Networks-for-Few-shot-Learning-PyTorch
+    Compute the prototypes by averaging the features of n_support
+    samples for each class in target, computes then the distances from each
+    samples' features to each one of the barycentres, computes the
+    log_probability for each n_query samples for each one of the current
+    classes, of appartaining to a class c, loss and accuracy are then computed
+    and returned
+    Args:
+    - input: the model output for a batch of samples
+    - target: ground truth for the above batch of samples
+    - n_support: number of samples to keep in account when computing
+      bprototypes, for each one of the current classes
+    '''
+
+    def supp_idxs(c):
+        return target_cpu.eq(c).nonzero()[:n_support].squeeze(1)
+
+    target_cpu = target.to('cpu')
+    input_cpu = input.to('cpu')
+    emb_dim = input_cpu.size(-1)
+    classes = torch.unique(target_cpu)
+    n_classes = len(classes)
+    p = n_classes * n_support
+    n_query = target.eq(classes[0].item()).sum().item() - n_support
+    support_idxs = list(map(supp_idxs, classes))
+    support_samples = torch.stack([input_cpu[idx_list] for idx_list in support_idxs])
+
+    prototypes = torch.stack([input_cpu[idx_list].mean(0) for idx_list in support_idxs])
+    #print(prototypes.size())
+
+    num_batch = prototypes.shape[0]
+    num_proto = prototypes.shape[1]
+
+    query_idxs = torch.stack(list(map(lambda c: target.eq(c).nonzero()[n_support:], classes))).view(-1)
+    query_samples = input.cpu()[query_idxs]
+
+    reconquery_samples = recon.cpu()[query_idxs]
+    reconsupport_samples = recon.cpu()[support_idxs]
+
+    groundquery_samples = ground.cpu()[query_idxs]
+    groundsupport_samples = ground.cpu()[support_idxs]
+
+
+    dists = euclidean_dist(query_samples, prototypes)
+    logits = -dists
+
+    log_p_y = F.log_softmax(-dists, dim=1).view(n_classes, n_query, -1)
+
+    target_inds = torch.arange(0, n_classes)
+    target_inds = target_inds.view(n_classes, 1, 1)
+    target_inds = target_inds.expand(n_classes, n_query, 1).long()
+
+    loss_c = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
+    reconquery_loss = lambda_r * F.mse_loss(reconquery_samples, groundquery_samples)
+    reconsupport_loss = lambda_r * F.mse_loss(reconsupport_samples, groundsupport_samples)
+    loss = loss_c + reconquery_loss +reconsupport_loss
+    _, y_hat = log_p_y.max(2)
+
+    acc_val = y_hat.eq(target_inds.squeeze()).float().mean()
+    #print(test)
+    return loss, acc_val
+
 
 def get_probability(proto_pos, neg_proto, query_set_out):
     """Calculates the  probability of each query point belonging to either the positive or negative class
      Args:
      - x_pos : Model output for the positive class
-     - neg_proto : Negative class prototype calculated from randomly chosed 100 segments across the audio file
+     - neg_proto : Negative class prototype calculated from randomly chosed 50 segments across the audio file
      - query_set_out:  Model output for the first 8 samples of the query set
 
      Out:
@@ -127,9 +191,8 @@ def evaluate_prototypes(conf=None, hdf_eval=None, device=None, strt_index_query=
 
     gen_eval = Datagen_test(hdf_eval, conf)
     X_pos, X_neg, X_query, hop_seg = gen_eval.generate_eval()
-
+    #print(X_neg.size())
     X_pos = torch.tensor(X_pos)
-
     Y_pos = torch.LongTensor(np.zeros(X_pos.shape[0]))
     X_neg = torch.tensor(X_neg)
     Y_neg = torch.LongTensor(np.zeros(X_neg.shape[0]))
@@ -144,7 +207,11 @@ def evaluate_prototypes(conf=None, hdf_eval=None, device=None, strt_index_query=
     query_set_feat = torch.zeros(0, 48).cpu()
     batch_samplr_pos = EpisodicBatchSampler(Y_pos, 2, 1, conf.train.n_shot)
     pos_dataset = torch.utils.data.TensorDataset(X_pos, Y_pos)
+
     pos_loader = torch.utils.data.DataLoader(dataset=pos_dataset, batch_sampler=None)
+
+
+
 
     if conf.train.encoder == 'Resnet':
         encoder = ResNet()
@@ -162,22 +229,31 @@ def evaluate_prototypes(conf=None, hdf_eval=None, device=None, strt_index_query=
     encoder.to(device)
     encoder.eval()
 
-    'List for storing the combined probability across all iterations'
-    prob_comb = []
+    print("Get feature dimension")
+    for batch in tqdm(pos_loader):
+        x_t, y_t = batch
+        tfeat = encoder(x_t.cuda())
+        if tfeat is True:
+            break
+    tfeatshape = tfeat.shape
+    emb_dim = tfeatshape[1]
 
-    emb_dim = 512
+    'List for storing the combined probability across all iterations'
+    #emb_dim = 512
+    prob_comb = []
 
     pos_set_feat = torch.zeros(0, emb_dim).cpu()
 
     print("Creating positive prototype")
     for batch in tqdm(pos_loader):
         x, y = batch
+        #print(x.size())
         feat = encoder(x.cuda())
         feat = feat.cpu()
         feat_mean = feat.mean(dim=0).unsqueeze(0)
-        print(feat_mean.shape)
         pos_set_feat = torch.cat((pos_set_feat, feat_mean), dim=0)
     pos_proto = pos_set_feat.mean(dim=0)
+
 
     iterations = conf.eval.iterations
     for i in range(iterations):
@@ -229,3 +305,7 @@ def evaluate_prototypes(conf=None, hdf_eval=None, device=None, strt_index_query=
 
     assert len(onset) == len(offset)
     return onset, offset
+
+
+
+
